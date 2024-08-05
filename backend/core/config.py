86a391433ef9d9ed65c849ebdf4c4b5ba12151e0
mrmim <mm.ahmadi0101@gmail.com>
2024-08-05 14:21:56 +0330

    add screenshots

--- backend/core/config.py
+++ backend/core/config.py
@@ -3,11 +3,11 @@ import os
 
 DIFFICULTY = 4 # Difiiculty of PoW algorithm
 FEE_PER_TRANSACTION = 0.01  # 0.01% of amount
-COINBASE_BENEFICIARY = os.environ.get('COINBASE_BENEFICIARY', '1ad4154a06f1126a504737f501fd05e1e76eecd9538ae1f497ca31144e9ec96aa59d4fe155888a07af75321ad45309596fd8dd48ef59414bb134968b6b0d7f42') # Wallet of the current miner
+COINBASE_BENEFICIARY = os.environ.get('COINBASE_BENEFICIARY', 'b3b6eedc5d6967c94aa2d65f9e0b77f34d9a7cde2b699d2b9039776cc92519900ee89b76732fc210ee91120b0dece9cef9518ad30d906c0b8f78919b6e87219c') # Wallet of the current miner
 
 ### Do Not Change These ###
 TRANSACTIONS_COUNT_PER_BLOCK = 3 # Number of transaction per block + Block reward transaction
 RANGE_BLOCK_SIZE_B = 2048  # 2048 Bytes
 MAX_TRANSACTION_AMOUNT = 21000000 # Transaction safety
-SUBSIDY_HALVING_INTERVAL = 2 # To calculate block subsidy
+SUBSIDY_HALVING_INTERVAL = 2100000 # To calculate block subsidy
 ROUND_AMOUNT = 8 # To round float numbers
